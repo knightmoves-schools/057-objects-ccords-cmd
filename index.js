87a5530bb5341bb1c083686baa0bc1214fbe746e
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-12-01 13:19:51 -0600

    Fix template literal for accessing school property

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ let school = {
   'average class size': 21,
   'mascot': "clown",
 };
-let description = `${school.name} has an average class size of ${school[average class size]} and their mascot is a ${school.mascot}`;
+let description = `${school.name} has an average class size of ${school[`average class size`]} and their mascot is a ${school.mascot}`;
 document.getElementById('result').innerHTML = description;
+
 
 
 
